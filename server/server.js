66da59fd8ed70fdb9ee1d78a093263e8f501f4b5
Timothy Yoon <tim.keun.yoon@gmail.com>
2017-08-07 11:00:07 -0700

    Server reconfigured to use socket

--- server/server.js
+++ server/server.js
@@ -1,16 +1,23 @@
 const express = require('express');
+const http = require('http');
 const parser = require('body-parser');
+const socketIo = require('socket.io');
+
 const PORT = 3000;
 const App = express();
+const server = http.createServer(App);
+const io = socketIo(server);
 
-App.use(parser.json());
-App.use(parser.urlencoded({extended: true}));
-
-
-App.listen(PORT, (err) => {
+App.use(express.static('client'));
+App.use(parser.urlencoded({extended: false}));
+server.listen(3000, (err) => {
     if(err){
-        console.log('Error setting up server.js')
+        console.log('error');
     } else {
-        console.log('Server.js is up and listening to Port:', PORT);
+        console.log('listening on 3000');
     }
+});
+
+io.on('connection', socket => {
+    console.log(connected);
 })
