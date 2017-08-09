ab039c9947d410f6161635209abe9fa5c2b5dc7e
brickbite <jeffreylee0116@gmail.com>
2017-08-09 07:33:13 -0700

    added possible action check to interface

--- client/src/components/playerinterface.jsx
+++ client/src/components/playerinterface.jsx
@@ -6,6 +6,8 @@ class Playerinteface extends React.Component {
     this.state = {
       gamestate: this.props.gamestate,
     }
+
+    this.takeAction = this.takeAction.bind(this);
   }
 
   componentWillReceiveProps(nextprops) {
@@ -15,18 +17,40 @@ class Playerinteface extends React.Component {
     });
   }
   
-  buy(event) {
+  takeAction(event) {
     var item = event.target.id;
     if (item === 'buyroad') {
-      console.log('buy: player wants to buy: ', item);
+      // console.log('button: player wants to: ', item);
       this.props.buymethod(item);
     } else if (item === 'buysettlement') {
-      console.log('buy: player wants to buy: ', item);
+      // console.log('button: player wants to: ', item);
+      this.props.buymethod(item);
     } else if (item === 'buycity') {
-      console.log('buy: player wants to buy: ', item);
+      // console.log('button: player wants to: ', item);
+      this.props.buymethod(item);
     } else if (item === 'buydevcard') {
-      console.log('buy: player wants to buy: ', item);
+      // console.log('button: player wants to: ', item);
+      this.props.buymethod(item);
+    } else if (item === 'offertrade') {
+      // console.log('button: player wants to: ', item);
+      this.props.offertrademethod();
+    } else if (item === 'playcardknight') {
+      // console.log('button: player wants to: ', item);
+      this.props.playcardmethod(item);
+    } else if (item === 'playcardroad') {
+      // console.log('button: player wants to: ', item);
+      this.props.playcardmethod(item);
+    } else if (item === 'playcardmonopoly') {
+      // console.log('button: player wants to: ', item);
+      this.props.playcardmethod(item);
+    } else if (item === 'playcardplenty') {
+      // console.log('button: player wants to: ', item);
+      this.props.playcardmethod(item);
+    } else if (item === 'playcardvictory') {
+      // console.log('button: player wants to: ', item);
+      this.props.playcardmethod(item);
     } 
+
 
   }
 
@@ -35,29 +59,29 @@ class Playerinteface extends React.Component {
     return (
       <div>
         <h3>Player Actions Menu</h3>
-        {(this.state.gamestate.ableToBuyRoad) ? <button type="button" id="buyroad" onClick={this.buy}>Buy Road</button> : null}
+        {(this.state.gamestate.ableToBuyRoad) ? <button type="button" id="buyroad" onClick={this.takeAction}>Buy Road</button> : null}
 
-        {(this.state.gamestate.ableToBuySettlement) ? <button type="button" id="buysettlement" onClick={this.buy}>Buy Settlement</button> : null}
+        {(this.state.gamestate.ableToBuySettlement) ? <button type="button" id="buysettlement" onClick={this.takeAction}>Buy Settlement</button> : null}
 
-        {(this.state.gamestate.ableToBuyCity) ? <button type="button" id="buycity" onClick={this.buy}>Buy City</button> : null}
+        {(this.state.gamestate.ableToBuyCity) ? <button type="button" id="buycity" onClick={this.takeAction}>Buy City</button> : null}
 
-        {(this.state.gamestate.ableToBuyDevelopmentCard) ? <button type="button" id="buydevcard" onClick={this.buy}>Buy Development Card</button> : null}
+        {(this.state.gamestate.ableToBuyDevelopmentCard) ? <button type="button" id="buydevcard" onClick={this.takeAction}>Buy Development Card</button> : null}
 
-        {(this.state.gamestate.ableToOfferTrade) ? <button type="button" id="offertrade" onClick={console.log('button: offerTrade pressed')}>Offer Trade</button> : null}
+        {(this.state.gamestate.ableToOfferTrade) ? <button type="button" id="offertrade" onClick={this.takeAction}>Offer Trade</button> : null}
 
-        {(this.state.gamestate.ableToPlayCardKnight) ? <button type="button" id="playcardknight" onClick={console.log('button: playcardknight pressed')}>Play Card: Knight</button> : null}
+        {(this.state.gamestate.ableToPlayCardKnight) ? <button type="button" id="playcardknight" onClick={this.takeAction}>Play Card: Knight</button> : null}
 
-        {(this.state.gamestate.ableToPlayCardRoad) ? <button type="button" id="playcardroad" onClick={console.log('button: playcardroad pressed')}>Play Card: Road Building</button> : null}
+        {(this.state.gamestate.ableToPlayCardRoad) ? <button type="button" id="playcardroad" onClick={this.takeAction}>Play Card: Road Building</button> : null}
 
-        {(this.state.gamestate.ableToPlayCardMonopoly) ? <button type="button" id="playcardmonopoly" onClick={console.log('button: playcardmonopoly pressed')}>Play Card: Monopoly</button> : null}
+        {(this.state.gamestate.ableToPlayCardMonopoly) ? <button type="button" id="playcardmonopoly" onClick={this.takeAction}>Play Card: Monopoly</button> : null}
 
-        {(this.state.gamestate.ableToPlayCardPlenty) ? <button type="button" id="playcardplenty" onClick={console.log('button: playcardplenty pressed')}>Play Card: Plenty</button> : null}
+        {(this.state.gamestate.ableToPlayCardPlenty) ? <button type="button" id="playcardplenty" onClick={this.takeAction}>Play Card: Plenty</button> : null}
 
-        {(this.state.gamestate.ableToPlayCardVictory) ? <button type="button" id="playcardvictory" onClick={console.log('button: playcardvictory pressed')}>Play Card: Victory Point</button> : null}
+        {(this.state.gamestate.ableToPlayCardVictory) ? <button type="button" id="playcardvictory" onClick={this.takeAction}>Play Card: Victory Point</button> : null}
 
-        {(this.state.gamestate.isInMenu) ? <button type="button" id="cancelaction" onClick={console.log('button: cancel action pressed')}>Cancel Action</button> : null}
+        {(this.state.gamestate.isInMenu) ? <button type="button" id="cancelaction" onClick={this.takeAction}>Cancel Action (isInMenu)</button> : null}
 
-        {(this.state.gamestate.ableToCancelAction) ? <button type="button" id="cancelaction" onClick={console.log('button: cancel action pressed')}>Cancel Action</button> : null}
+        {(this.state.gamestate.ableToCancelAction) ? <button type="button" id="cancelaction" onClick={this.takeAction}>Cancel Action (ableToCancel)</button> : null}
 
         {(!this.state.gamestate.hasRolled) ? <button type="button" onClick={this.props.diceRoll}>Roll Dice</button> : null}
 
