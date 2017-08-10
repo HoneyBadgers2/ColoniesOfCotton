ad1613bc1e2247bdea9d2c90f79cc9b201219b37
Michael <miken619@gmail.com>
2017-08-09 19:41:23 -0700

    Added ships, ports, and robber to the board. Add functionality to move robber by clickong on a resource tile

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -10,61 +10,118 @@ export default class App extends Component {
 
         this.scene = undefined;
         this.engine = undefined;
-        
         this.onSceneMount = this.onSceneMount.bind(this);
         this.onMeshPicked = this.onMeshPicked.bind(this);
         this.initEnvironment = this.initEnvironment.bind(this)
         this.getIDFromMesh = this.getIDFromMesh.bind(this);
+        this.moveRobber = this.moveRobber.bind(this);
+        this.placeRoad = this.placeRoad.bind(this);
+        this.placeHouseAndCity = this.placeHouseAndCity.bind(this);
+        this.showPossibleRoadAndHousePlacement = this.showPossibleRoadAndHousePlacement.bind(this);
+
+        this.player1 = 1;
+        this.player2 = 2;
+        this.player3 = 3;
+        this.player3 = 4;
+
+
 
      }
 
     
     onMeshPicked(mesh, scene) {
         // This will be called when a mesh is picked in the canvas
-        console.log(mesh.name);
-        var redMat = new StandardMaterial("floor", scene);
-        redMat.diffuseColor = new Color3(1, 0, 0);
-        //redMat.specularColor = new Color3(1, 0, 0);
-        redMat.emissiveColor = Color3.Red();
-        if (mesh != null && mesh.name.includes('House')){             
-            mesh.material = redMat;             
-            if(mesh.visibility > 0) {
-                var city = 'City' + this.getIDFromMesh(mesh.name);
-                
-                var garage = scene.getMeshByID(city);  
-                garage.material = redMat;   
-                garage.visibility = 1;
-            } 
-                 mesh.visibility = 1;           
+       
+        if(mesh != null){
+            
+            //TODO: Put code into a condition and will be use to put materials on roads and houses/cities. 
+            //Material color will be dependent on whose turn it is.
+            var mat = new StandardMaterial("Color", scene);
+
+            //Red
+            //mat.diffuseColor = new Color3(1, 0, 0);
+
+            //Yellow
+            //mat.diffuseColor = new Color3(1, 1, 0);
+
+            //blue
+            mat.diffuseColor = new Color3(0, 0, 1);
+
+
+            //Makes a house appear when click makes a city appear when click twice
+            if (mesh.name.includes('House')){             
+               this.placeHouseAndCity(mesh, mat, scene);    
+               this.showPossibleRoadAndHousePlacement(mesh);
+ 
+            }
+
+            if (mesh.name.includes('Road')){
+                this.placeRoad(mesh, mat);
+                this.showPossibleRoadAndHousePlacement(mesh);
+
+
+            }
+
+            //Move the robber on a resource tile
+            if(mesh.name.includes('Rock') || mesh.name.includes('Hay') || 
+               mesh.name.includes('Brick') || mesh.name.includes('Sheep') || 
+               mesh.name.includes('Tree')) {
+                this.moveRobber(mesh, scene);
+            }
+
+          
+
         }
-        if (mesh != null && mesh.name.includes('Road')){
-            mesh.visibility = 1;   
-            mesh.material = redMat;
-
-        }  
-
+       
         
     }
-    
+
+    placeHouseAndCity(mesh, mat, scene){
+        mesh.material = mat;             
+        if(mesh.visibility > 0) {
+            var city = 'City' + this.getIDFromMesh(mesh.name);
+            var city = scene.getMeshByID(city);  
+            city.material = mat;   
+            city.visibility = 1;
+        } 
+            mesh.visibility = 1;  
+
+    }
+
+    moveRobber(mesh, scene) {
+        var robber = scene.getMeshByID('Robber'); 
+        robber.position.x = mesh.position.x;
+        robber.position.z = mesh.position.z;
+    }
+
+    placeRoad(mesh, mat) {
+        mesh.visibility = 1;   
+        mesh.material = mat;
+    }
+
+    showPossibleRoadAndHousePlacement(mesh){
+        mesh.visibility = 0.5;
+    }
+
     getIDFromMesh(name){
         return name.slice(-2);
     }
+
+
     
     onSceneMount(e) {
         const { canvas, scene, engine} = e;   
         this.scene = scene;   
         this.engine = engine;
+        console.log('Engine is: ' + this.engine);
         this.initEnvironment(canvas, scene);
-        SceneLoader.ImportMesh("", "", "boardTemplate.babylon", scene, function (newMeshes) {
-          
-            var garage;
+        SceneLoader.ImportMesh("", "", "boardTemplate.babylon", scene, function (newMeshes) {       
             for(var mesh of newMeshes) {
                 mesh.convertToFlatShadedMesh();
-
                 if(mesh.name.includes('City') || mesh.name.includes('House') || mesh.name.includes('Road')) {       
                     mesh.visibility = 0.0;                                
-                }           
-            }    
+                } 
+            }              
         });
        
 
