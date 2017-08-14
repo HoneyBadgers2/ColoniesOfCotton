6d39c6127ea9c5fc83f9a11b94b2a64224f3e128
Aren Rostamian <arenrostamian@gmail.com>
2017-08-13 20:25:04 -0700

    Here be dragonz

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Lobby from './components/Lobby.jsx';
+import Game from './components/Game.jsx';
 import App from './components/App.jsx';
 
-ReactDOM.render(<Lobby />, document.getElementById('app'));
+ReactDOM.render(<Game />, document.getElementById('app'));
 document.addEventListener('contextmenu', event => event.preventDefault());
