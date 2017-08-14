f9f50c677463c9644b4c5e5c779ef7a215f9a47c
Aren Rostamian <arenrostamian@gmail.com>
2017-08-13 20:15:44 -0700

    Fix for Heroku

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const socketIo = require('socket.io');
 const db = require('../db/mongo');
 const data = require('../data.json');
 
-const PORT = 3000;
+const port = process.env.PORT || 3000;
 const App = express();
 const server = http.createServer(App);
 const io = socketIo(server);
@@ -14,7 +14,7 @@ const initial = require('../data.json');
 App.use(express.static('client'));
 App.use(parser.urlencoded({extended: false}));
 App.use(parser.json());
-server.listen(3000, (err) => {
+server.listen(port, (err) => {
     if(err){
         console.log('error');
     } else {
